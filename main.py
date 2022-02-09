c551b9522107313520b15221df812cb4329414ff
lea.rieping <lea.rieping@gsits.de>
2022-02-09 14:38:36 +0100

    Get all Users implementiert

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-from flask import Flask
+
+from flask import Flask, Response
+from json import dumps
+from model.User import User
+from model.Entry import Entry
+
+def default(o):
+    return o._asdict()
+
+users = [
+    User("Rieping", "Lea", "LR", "Lea.Rieping@outlook.de", 1)
+]
 
 app = Flask(__name__)
+
+@app.route('/v1/users', methods = ['GET'])
+def getAllUsers():
+
+    return Response(dumps(users, default=default), status=200, mimetype='application/json')
+
+if __name__ == '__main__':
+    # starte Flask-Server
+    app.run(host='localhost', port=5000)
+
+
 
--- model/User.py
+++ model/User.py
@@ -1,5 +1,12 @@
 class User:
 
-    def __init__(self, name: str, firstName: str):
+    def __init__(self, name: str, firstName: str, username: str, email: str, id: int):
         self.name = name
         self.firstName = firstName
+        self.username = username
+        self.email = email
+        self.id = id
+
+    
+    def _asdict(self):
+        return self.__dict__
